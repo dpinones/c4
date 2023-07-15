fdb75246895f2df268675f8791ddb249a3cb723b
dpinones <pinonesdamian@gmail.com>
2023-07-15 15:58:14 +0200

    add starknet.js and env

--- browser/src/app.ts
+++ browser/src/app.ts
@@ -2,6 +2,18 @@ import * as Game from './game'
 import { Board } from './board'
 import './style.css'
 
+import { Contract, Provider, json, ec, Account, constants, stark, uint256, CallData, cairo, Call } from "starknet";
+import { connect, disconnect } from "get-starknet";
+
+let walletConnected: boolean | undefined;
+let walletAddress: string | undefined;
+let walletProvider: any | undefined;
+
+let MECHA_STARK_WALLET_PRIVKEY : string | undefined;
+let MECHA_STARK_WALLET_ACC_ADDRESS: string | undefined;
+let FOUR_CONNECT_STARK_ONLINE_ADDRESS: string | undefined;
+let FOUR_CONNECT_STARK_AI_ADDRESS: string | undefined;
+
 document.addEventListener('DOMContentLoaded', () => {
   const canvas = document.querySelector('.section-canvas') as HTMLCanvasElement
 
@@ -63,18 +75,48 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   function initGame(chosenMode: string | null) {
-    console.log('initGame chosenMode:', chosenMode)
-    backToModeSelector?.classList.remove('hidden')
-    if (chosenMode === 'offline-human') {
-      currentGameHandler = Game.initGameLocal2p()
-    } else if (chosenMode === 'offline-ai') {
-      currentGameHandler = Game.initGameLocalAi()
-    } else if (chosenMode === 'online-human') {
-      currentGameHandler = Game.initGameOnline2p()
-    } else if (chosenMode === 'ai-vs-ai') {
-      currentGameHandler = Game.initGameAiVsAi()
-    } else {
-      console.error('Invalid game mode received', chosenMode)
+
+    console.log('import.meta.env.VITE_FOUR_CONNECT_STARK_WALLET_ACC_ADDRESS: ', import.meta.env.VITE_FOUR_CONNECT_STARK_WALLET_ACC_ADDRESS);
+
+    // conectar la wallet
+    connectWallet()
+      .then(() => {
+        console.log('Wallet connected')
+        
+        // crear game
+
+        console.log('initGame chosenMode:', chosenMode)
+        backToModeSelector?.classList.remove('hidden')
+        // if (chosenMode === 'offline-human') {
+        //   currentGameHandler = Game.initGameLocal2p()
+        // } 
+        // else if (chosenMode === 'ai-vs-ai') {
+        //   currentGameHandler = Game.initGameAiVsAi()
+        // } 
+        if (chosenMode === 'offline-ai') {
+          currentGameHandler = Game.initGameLocalAi()
+        } else if (chosenMode === 'online-human') {
+          currentGameHandler = Game.initGameOnline2p()
+        } else {
+          console.error('Invalid game mode received', chosenMode)
+        }
+      })
+      .catch((err) => {
+        console.error('Wallet connection error', err)
+      })
+  }
+
+  // STARKNET 
+  async function connectWallet() {
+    if (!walletConnected) {
+      const wallet = await connect({ modalMode: "alwaysAsk", modalTheme: "dark", storeVersion: "chrome" });
+      if (wallet) {
+        await wallet.enable();
+        walletConnected = wallet.isConnected;
+        walletAddress = wallet.selectedAddress;
+        walletProvider = wallet.account;
+      }
     }
   }
+
 })
